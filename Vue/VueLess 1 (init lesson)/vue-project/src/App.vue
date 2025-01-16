ccc5a464804cd74810639d40bb0a30bc4cc752ba
ray3en <tigranlee@gmail.com>
2025-01-16 19:05:07 +0400

    16.01

--- Vue/VueLess 1 (init lesson)/vue-project/src/App.vue
+++ Vue/VueLess 1 (init lesson)/vue-project/src/App.vue
@@ -1,71 +1,53 @@
-
-<!--  binding (биндинг)
- Для формирования текстового содержимого используются {{ value }} 
- Для формирования выражения внутри атрибутов используется знак : вначале атрибута. Далее в кавычках можно указать JS выражение
- Для стилизации важно указать в кавычках объект (если надо указать JS выражение), перечислив значение стилизации как набор ключ-значение по указанному примеру
--->
- 
-<!-- Директивы
- v-for (директива для создания цикла) 
- v-if  (условная отрисовка)
--->
 <template>
-  <h1 class="main_text">Hello world!</h1>
-  <p>{{ text }}</p>
-  <p :class='classExample + "dered"'>{{ price }}</p>
-  <p :style="{display:  (isShow) ? 'block' : 'none', fontSize: '50px'}">Hidden</p>
-
-  <!-- Пример цикла v-for-->
-  <ul>
-    <li v-for="(item, ind) in products" :key="ind">
-      {{ item }}
-    </li>
-  </ul>
-
-  <!-- Пример условной отрисовки v-if -->
-  <div v-if="isShow">Услованя отрисовка</div>
-  <div v-else-if="price > 3000">Услованая отрисовка (price)</div>
-  <div v-else>Услованя отрисовка (false)</div>
-
-
   <div>
-
+    <h2>Binding & Derictive example</h2>
+    <DirectiveBind></DirectiveBind>
+  </div>
+  <div>
+    <h2>Task 1</h2>
+    <div class="post_wrapper">
+      <Post
+        v-for="item in posts"
+        :key="item.id"
+        :title="item.title"
+        :text="item.text"
+      ></Post>
+    </div>
   </div>
 </template>
 
-<script>
-// Все переменные инициализируются внутри метода data() который возвращает объект с наборами ключ-значение 
-// где ключ - имя переменной, а значение - его значение (число, строка, массив, булевый тип и тд)
+<!-- Задание 
+  Создайте цикл по массиву posts и сформируйте 4 компонента Post передав пропы title и text
+  В качестве ответа отправьте содержимое компонента App
+  -->
 
-  export default {
-    data(){
-      return {
-        text: 'Some text!',
-        price: 2000,
-        classExample: 'bor',
-        isShow: false,
-        products: ["Шоколад", "Пончик","Жвачка", "Мармелад", "Газировка"],
-        imgSrc: 'https://i.pinimg.com/originals/25/ef/a7/25efa79cb9e70f6af08cb47f851b8ebf.png'
-      }
+<script>
+import DirectiveBind from './components/DirectiveBind.vue';
+import Post from './components/Post.vue';
+
+export default {
+  components: {
+    DirectiveBind, Post
+  },
+  data(){
+    return {
+      posts: [
+        {id: 1, title: 'Title 1', text: 'Text 1'},
+        {id: 2, title: 'Title 2', text: 'Text 2'},
+        {id: 3, title: 'Title 3', text: 'Text 3'},
+        {id: 4, title: 'Title 4', text: 'Text 4'},
+      ]
     }
   }
+}
 </script>
 
-
-<!-- scoped - изолированная стилизация (модульная) -->
 <style scoped>
-.bordered{
-  padding: 10px;
-  border: 1px solid black;
+.post_wrapper{
+  margin: 10px;
+  display: flex;
+  gap: 15px;
+  flex-wrap: wrap;
+  justify-content: center;
 }
-
-.main_text {
-  color: red
-}
-
-.second_text{
-  color: purple;
-}
-
 </style>
-
