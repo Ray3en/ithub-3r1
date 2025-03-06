242f3d5efba3d0343c30d07ebc88a9c233dffb7a
ray3en <tigranlee@gmail.com>
2025-03-06 20:41:32 +0400

    06.03

--- Angular/AngularLess_1(init)/myapp/src/app/app.component.ts
+++ Angular/AngularLess_1(init)/myapp/src/app/app.component.ts
@@ -2,10 +2,18 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UserCardComponent } from './components/user-card/user-card.component';
 import { NumberCardComponent } from './components/number-card/number-card.component';
+import { NgStyle } from '@angular/common';
+import { PipeExampleComponent } from './components/pipe-example/pipe-example.component';
+
+type UserType = {
+  id: number,
+  name: string,
+  email: string,
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, UserCardComponent, NumberCardComponent],
+  imports: [RouterOutlet, UserCardComponent, NumberCardComponent, PipeExampleComponent,NgStyle],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -17,8 +25,16 @@ import { NumberCardComponent } from './components/number-card/number-card.compon
 // В каетве ответа прислать код разметки компонента number-card
 
 export class AppComponent {
-  title = 'myapp';
-  number = 6
-  name = 'Tigran'
-  email = 'movsisyantv@ithub.ru'
+  themeClass:string = 'dark'
+  title :string = 'myapp';
+  number :number = 10
+
+  users: UserType[] = [
+    {id: 1, name: 'Alex', email: 'alex@ithub.ru'},
+    {id: 2, name: 'Willyam', email: 'willyam.tit@ithub.ru'},
+    {id: 3, name: 'Steven', email: 'steven.man@ithub.ru'},
+    {id: 4, name: 'Marry', email: 'marry_example@ithub.ru'},
+  ]
+
 }
+
